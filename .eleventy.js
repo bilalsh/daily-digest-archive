bc687c1c09f333a9f6ff71bed5708001f5da2756
Bilal Shehzad <6753669+bilalsh@users.noreply.github.com>
2026-09-05 12:26:11 +0500

    feat: redesign digest archive

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
 
+  eleventyConfig.addFilter("formatDate", function (dateString) {
+    const date = new Date(`${dateString}T00:00:00`);
+    return date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  });
+
   return {
     dir: {
       input: "src",
